53b7e9f5d232ad593b7fd567f3a9c77923725008
Camilo Ribeiro <camilo@camiloribeiro.com>
2013-11-21 00:45:54 +0100

    adding first methods and tests

--- lib/icecream/parser.rb
+++ lib/icecream/parser.rb
@@ -1,10 +1,24 @@
 
 module IceCream
   class Parser
+
+    def initialize(path)
+      @path = path
+      @file = File.read path
+    end
+
+    def get_flavor_name
+        @title = slice_between_strings(@path, "/flavors/", ".flavor")
+    end
     def read
-      @file = File.read(File.join(File.dirname(__FILE__),"../../spec/flavors/chocolate.rb")) 
+      @file
     end
     def parse
+    end
+    def slice_between_strings(string, str_start, str_end)
+      start_at = string.index(str_start).to_i + str_start.size
+      end_at = string.index(str_end)
+      string = string.slice start_at..end_at-1
     end
     def objetify
     end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,10 +3,17 @@ require File.join(File.dirname(__FILE__),"../lib/icecream/parser")
 describe "Parser" do
   describe "Parsing a file into an array" do
     before(:all) do
-      @parser = IceCream::Parser.new
+      @parser = IceCream::Parser.new File.join(File.dirname(__FILE__),"../spec/flavors/chocolate.flavor")
     end
     it "Reading the file" do
       @parser.read.should == "@name = \"chocolate\"\n@calories = 150\n@color = :brown\n@price = 15.5\n"
+    end
+    it "Gets the string between two strings" do
+      @parser.slice_between_strings("/super/flavors/chocolate.flavor", "/flavors/", ".flavor")
+      .should == "chocolate"
+    end
+    it "Gets the name of the future object" do
+      @parser.get_flavor_name
     end
   end
 end
