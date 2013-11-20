8c8fd26c4a838554829f9526c12c6f24a1dc52ad
camilo <camilo@camilodell.(none)>
2013-11-20 23:18:23 +0100

    adding rspec and rake

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem 'pry'
+gem 'rake'
+gem 'rspec'
+
+gemspec
---
+++ spec/element_spec.rb
@@ -0,0 +1,11 @@
+describe "IceCream" do
+  describe "Adding rspec" do
+    before(:all) do
+      @work = true
+    end
+    it "works" do
+      @work
+    end
+  end
+end
+
